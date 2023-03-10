98ccee9a06eecddb09b3d6d13c3618e71858adad
aksheoran098 <60999606+aksheoran098@users.noreply.github.com>
2023-03-10 18:02:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views' ) );
-app.use(express.urlencoded())
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 
 var contactList = [
